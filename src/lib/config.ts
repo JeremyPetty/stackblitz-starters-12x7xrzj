4aeef61d4daf3e6923daf1f88af10f20b8b8ca0b
JeremyPetty <33050140+JeremyPetty@users.noreply.github.com>
2025-09-10 15:52:38 -0700

    Add public URL constant for Google Apps Script

---
+++ src/lib/config.ts
@@ -0,0 +1,3 @@
+// It's fine to expose this; your Apps Script web app is already "Anyone with the link".
+export const GAS_PUBLIC_URL =
+  'https://script.google.com/macros/s/AKfycbwYy503qt1fJxbydFE6xFJxzrotCO7LzFNiiQHu8Zj2hQ4JLCa13bGGIOdYfX14ZC1nLQ/exec';
