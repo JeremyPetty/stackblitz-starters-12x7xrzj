b392f6f865f134874f241dd4fb20ae820f85b6e6
JeremyPetty <33050140+JeremyPetty@users.noreply.github.com>
2025-09-10 22:42:54 -0700

    Update GAS_PUBLIC_URL to new endpoint

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 // It's fine to expose this; your Apps Script web app is already "Anyone with the link".
 export const GAS_PUBLIC_URL =
-  'https://script.google.com/macros/s/AKfycbzFjMmTb2lHCE9w10cJv5y7XtGa495ffdfgsxfCZHC0o1xMDeNh_R3GllujM3WsHoWjrw/exec';
+  'https://script.google.com/macros/s/AKfycbw-rMgO_HsG1Bf8paBRTwLyNtn7OOk-gXo7vMKcMCOyTh6GszmEdHGvqE02BBZRVF-L/exec';
