e1c09a06d25df2cb5b5363895172e4ab8d64dd08
JeremyPetty <33050140+JeremyPetty@users.noreply.github.com>
2025-09-10 22:39:22 -0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 // It's fine to expose this; your Apps Script web app is already "Anyone with the link".
 export const GAS_PUBLIC_URL =
-  'https://script.google.com/macros/s/AKfycbwTHD8KDxyzAjP3B21ZFHz5en5RR9EL_olJGh98EUD1M_ACQAxRv4xwXA4W1coHIKgJig/exec';
+  'https://script.google.com/macros/s/AKfycbzFjMmTb2lHCE9w10cJv5y7XtGa495ffdfgsxfCZHC0o1xMDeNh_R3GllujM3WsHoWjrw/exec';
