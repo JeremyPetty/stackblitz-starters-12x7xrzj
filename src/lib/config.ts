06234fd26139ebcd5d8133384b3d9db02498f5b8
JeremyPetty <33050140+JeremyPetty@users.noreply.github.com>
2025-09-12 12:04:30 -0700

    Update GAS_PUBLIC_URL to new script URL
    Upadated to Noah's Email

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,3 @@
 // It's fine to expose this; your Apps Script web app is already "Anyone with the link".
 export const GAS_PUBLIC_URL =
-  'https://script.google.com/macros/s/AKfycbw-rMgO_HsG1Bf8paBRTwLyNtn7OOk-gXo7vMKcMCOyTh6GszmEdHGvqE02BBZRVF-L/exec';
+  'https://script.google.com/macros/s/AKfycbz9FL0_2_Z_8VE7BEduQO7IUJKZ9QhAL_V10eogPMQ5DcEbF6uHSCEf7F6XgD6Z5Dw9/exec';
